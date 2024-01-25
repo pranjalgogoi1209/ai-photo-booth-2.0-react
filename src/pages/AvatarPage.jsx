ef125d82039f8c2e93e891c1f1b3ccb3dbc5a685
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-25 09:51:10 +0530

    Avatar page responsive added for tablet, bigscreens

--- src/pages/AvatarPage.jsx
+++ src/pages/AvatarPage.jsx
@@ -10,12 +10,16 @@ export default function AvatarPage({ capturedImage }) {
     <AvatarPageWrapper>
       {/* header starts here */}
       <header>
-        <h1>Select Your Avatar</h1>
+        <div className="title">
+          <h1>Select Your Avatar</h1>
+        </div>
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
       </header>
       {/* header ends here */}
+
+      {/* main starts here */}
       <main>
         {superHeros &&
           superHeros.map((superHero, index) => (
@@ -28,25 +32,36 @@ export default function AvatarPage({ capturedImage }) {
             </div>
           ))}
       </main>
+      <footer>
+        <button>Submit</button>
+      </footer>
+      {/* main ends here */}
     </AvatarPageWrapper>
   );
 }
 
 const AvatarPageWrapper = styled.div`
+  /* width: 100vw; */
+  padding-bottom: 2vw;
   /* header starts here */
   header {
     border: 1px solid black;
     display: flex;
     justify-content: space-between;
-    h1 {
-      border: 1px solid black;
+    .title {
+      border: 1px solid red;
       flex: 1;
       display: flex;
       justify-content: center;
-      align-items: flex-end;
-      font-size: 3.5vw;
-      font-weight: 600;
+      align-items: center;
       padding-left: 10vw;
+      h1 {
+        border: 0.15vw solid black;
+        padding: 0.1vw 0.4vw;
+        font-size: 3vw;
+        font-weight: 600;
+        border-radius: 0.7vw;
+      }
     }
     .logo {
       width: 10vw;
@@ -65,17 +80,17 @@ const AvatarPageWrapper = styled.div`
     border: 1px solid red;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
+    justify-content: flex-start;
     gap: 1vw;
     flex-wrap: wrap;
-    height: 80vh;
-    width: 70vw;
-    margin: auto;
+    height: 90vh;
+    width: 70%;
+    margin: 1vw auto 0 auto;
     .singleSuperHero {
       border: 1px solid black;
       img {
-        width: 10vw;
+        width: 12vw;
       }
     }
     #mt {
@@ -83,4 +98,42 @@ const AvatarPageWrapper = styled.div`
     }
   }
   /* main ends here */
+
+  /* footer starts here */
+  footer {
+    border: 1px solid purple;
+    button {
+      border: none;
+      outline: none;
+      padding: 0.5vw 2vw;
+      font-weight: 600;
+      font-size: 1.5vw;
+      border-radius: 0.6vw;
+      cursor: pointer;
+      background-color: yellow;
+      margin: 0 auto;
+      display: block;
+    }
+  }
+  /* footer ends here */
+
+  /* for big screens */
+  @media screen and (min-width: 1024px) {
+    main {
+      height: 100vh;
+    }
+  }
+
+  /* for laptop screens */
+  @media screen and (max-width: 768px) {
+    background: url("./../src/assets/tablet-background-image.png");
+    background-position: center;
+    background-size: cover;
+    background-repeat: repeat;
+    height: 100vh;
+    /* border: 10px solid red; */
+    main {
+      height: 70vh;
+    }
+  }
 `;
